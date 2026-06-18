933b496936d13af9df1acfb017b739eb95157355
Aunish7 <aunishkewat28@gmail.com>
2026-06-19 00:33:20 +0530

    Initial commit
    New Project

---
+++ PhotoGallery/PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Aunish Jayprakash Kewat on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoGallery/PhotoGalleryTests/PhotoGalleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryTests.swift
+//  PhotoGalleryTests
+//
+//  Created by Aunish Jayprakash Kewat on 19/06/26.
+//
+
+import Testing
+@testable import PhotoGallery
+
+struct PhotoGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
