0849172a371c636f329e941cfae28b1d00905216
Aunish7 <aunishkewat28@gmail.com>
2026-06-19 21:10:22 +0530

    chore: initialize project structure and MVVM folders

---
+++ PhotoGallery/PhotoGallery/Views/PhotoListView.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoListView.swift
+//  PhotoGallery
+//
+//  Created by Aunish Jayprakash Kewat on 19/06/26.
+//
+
+import SwiftUI
+
+struct PhotoListView: View {
+    var body: some View {
+        NavigationView {
+            Text("Photo Gallery")
+                .navigationTitle("Photos")
+        }
+    }
+}
+
+#Preview {
+    PhotoListView()
+}
